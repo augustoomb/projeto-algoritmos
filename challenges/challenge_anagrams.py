f0c1300ebaeb71bc8368ad18eb490374f9a38520
augusto barbosa <augustobarbosa1988@gmail.com>
2023-01-25 19:32:00 -0300

    fix: correções lint para a função is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,20 +1,30 @@
-from unidecode import unidecode # remover acentos
+# remover acentos
+from unidecode import unidecode
+
 
 def is_anagram(first_string, second_string):
-    arr_primeira_str_ordenada = quick_sort(list(unidecode(first_string).lower()))
-    arr_segunda_str_ordenada = quick_sort(list(unidecode(second_string).lower()))
+    arr_primeira_str_ordenada = quick_sort(
+        list(unidecode(first_string).lower())
+    )
+    arr_segunda_str_ordenada = quick_sort(
+        list(unidecode(second_string).lower())
+    )
     primeira_str = ''.join(arr_primeira_str_ordenada)
     segunda_str = ''.join(arr_segunda_str_ordenada)
     comparacao = primeira_str == segunda_str
-    return(primeira_str, segunda_str, comparacao)
+    return (primeira_str, segunda_str, comparacao)
 
-# Baseado em: https://stackoverflow.com/questions/48017422/implementing-quick-sort-in-python
+
+# Baseado em: https://stackoverflow.com/questions/48017422
+# /implementing-quick-sort-in-python
+
+
 def quick_sort(arr):
-    tam_arr = len(arr) # calcula o tamanho do array
+    tam_arr = len(arr)
     if tam_arr <= 1:
         return arr
-    meio_do_arr = tam_arr // 2 # calcula o meio do array
-    pivot = arr[meio_do_arr] # pega o elemento que está no meio
+    meio_do_arr = tam_arr // 2
+    pivot = arr[meio_do_arr]
     esq = [x for x in arr if x < pivot]
     dir = [x for x in arr if x > pivot]
     meio = [x for x in arr if x == pivot]
