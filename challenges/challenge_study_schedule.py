cf5f9afbaf2bb020281025ccffd058b8be26bc56
augusto barbosa <augustobarbosa1988@gmail.com>
2023-01-25 17:54:05 -0300

    feat: função is_palindrome_recursive: recebe uma string e devolve um booleano, informando se o string recebido é um palíndromo - recursiva

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -7,17 +7,5 @@ def study_schedule(permanence_period, target_time):
                 contador += 1
 
         return contador
-    # except ValueError:
-    #     return None
     except TypeError:
         return None
-
-
-# lista_estudantes = [1, 2, 3, 4, 5, 6]
-# lista_permanencia = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
-
-# print(study_schedule(lista_permanencia, 5))
-# print(study_schedule(lista_permanencia, 4))
-# print(study_schedule(lista_permanencia, 3))
-# print(study_schedule(lista_permanencia, 2))
-# print(study_schedule(lista_permanencia, 1))
