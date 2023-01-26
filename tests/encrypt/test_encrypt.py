ae449f4db380ecc7b125f1aabd656f7d2e0bf1ca
augusto barbosa <augustobarbosa1988@gmail.com>
2023-01-26 14:50:40 -0300

    fix: remoçao de texto incorreto dentro dos testes de test_encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -2,8 +2,7 @@ from challenges.challenge_encrypt_message import encrypt_message
 import pytest
 
 
-def test_encrypt_message():
-    pass
+def test_encrypt_message():    
     with pytest.raises(TypeError):
         encrypt_message(123, "não é um int")
 
