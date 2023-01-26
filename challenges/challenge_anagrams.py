aeee88d5bb5478a468e4b5ed07939077f217d5f5
augusto barbosa <augustobarbosa1988@gmail.com>
2023-01-25 21:12:06 -0300

    fix: refatoração na função is_anagram, responsável por retornar a comparação de 2 strings com a finalidade de identificar anagramas. Código foi refatorado, dividindo em funções menores, com menos responsabilidades

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -2,17 +2,43 @@
 from unidecode import unidecode
 
 
+def remove_accents(str):
+    return unidecode(str)
+
+
+def str_to_list(str):
+    return list(str)
+
+
+def str_to_lower_case(str):
+    return str.lower()
+
+
+def mount_list(str):
+    str_without_accents = remove_accents(str)
+    str_lower_case = str_to_lower_case(str_without_accents)
+    list = str_to_list(str_lower_case)
+    return list
+
+
+def list_to_str(list):
+    return ''.join(list)
+
+
+def compare_strings(str1, str2):
+    if str1 == '' and str2 == '':
+        return False
+    else:
+        return str1 == str2
+
+
 def is_anagram(first_string, second_string):
-    arr_primeira_str_ordenada = quick_sort(
-        list(unidecode(first_string).lower())
-    )
-    arr_segunda_str_ordenada = quick_sort(
-        list(unidecode(second_string).lower())
-    )
-    primeira_str = ''.join(arr_primeira_str_ordenada)
-    segunda_str = ''.join(arr_segunda_str_ordenada)
-    comparacao = primeira_str == segunda_str
-    return (primeira_str, segunda_str, comparacao)
+    arr_primeira_str_ordenada = quick_sort(mount_list(first_string))
+    arr_segunda_str_ordenada = quick_sort(mount_list(second_string))
+    primeira_str_ordenada = list_to_str(arr_primeira_str_ordenada)
+    segunda_str_ordenada = list_to_str(arr_segunda_str_ordenada)
+    comparacao = compare_strings(primeira_str_ordenada, segunda_str_ordenada)
+    return (primeira_str_ordenada, segunda_str_ordenada, comparacao)
 
 
 # Baseado em: https://stackoverflow.com/questions/48017422
@@ -31,4 +57,5 @@ def quick_sort(arr):
     return quick_sort(esq) + meio + quick_sort(dir)
 
 
-# print(is_anagram("Augusto", "OtsuguA"))
+# print(is_anagram("Augusto", "Otávio"))
+# print(is_anagram("", ""))
