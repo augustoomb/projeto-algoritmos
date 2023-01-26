2f5379c95f52b2400a3ebc8610cfeae953b032e1
augusto barbosa <augustobarbosa1988@gmail.com>
2023-01-26 15:06:35 -0300

    fix: correção no teste test_encrypt_message. Asserção incorreta

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -2,12 +2,12 @@ from challenges.challenge_encrypt_message import encrypt_message
 import pytest
 
 
-def test_encrypt_message():    
+def test_encrypt_message():
     with pytest.raises(TypeError):
         encrypt_message(123, "não é um int")
 
-    assert encrypt_message("BCAPAC", "não é um indice valido") == "CAPACB"
+    assert encrypt_message("BCAPAC", -1) == "CAPACB"
 
-    assert encrypt_message("BCAPAC", 3) == "CAP_ACB"
+    assert encrypt_message("BCAPAC", 3) == "ACB_CAP"
 
     assert encrypt_message("BCAPAC", 4) == "CA_PACB"
