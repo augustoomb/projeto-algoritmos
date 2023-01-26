9d5c3f4f3b225693f5eb5ab6b658b29c0e4ec58f
augusto barbosa <augustobarbosa1988@gmail.com>
2023-01-26 14:41:18 -0300

    tests: testes para a função encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
     pass
+    with pytest.raises(TypeError):
+        encrypt_message(123, "não é um int")
+
+    assert encrypt_message("BCAPAC", "não é um indice valido") == "CAPACB"
+
+    assert encrypt_message("BCAPAC", 3) == "CAP_ACB"
+
+    assert encrypt_message("BCAPAC", 4) == "CA_PACB"
